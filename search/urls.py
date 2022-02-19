b430b040e453f04331c2001f824fb30466c5952c
sbehmanesh <behmanesh1378@gmail.com>
2022-02-19 20:06:04 +0330

    google map to leafletJS

--- search/urls.py
+++ search/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.search, name='search'),
-    path('explorer', views.index, name='index')
+    path('search', views.search, name='search'),
+    path('', views.index, name='index')
 ]
